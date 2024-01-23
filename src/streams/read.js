a5fa26303c3048297f8f1ab7f0dd1e559b1dc3cc
Asmat1k <dobrovti@mail.ru>
2024-01-23 17:31:20 +0300

    feat: add read task solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import process from 'process'
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = `${__dirname}/files/fileToRead.txt`;
+
+    const readableStream = fs.createReadStream(filePath, 'utf-8');
+    readableStream.on('error', (error) => {
+        console.log(`ERROR: ${error}`);
+    })
+    readableStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    })
 };
 
 await read();
