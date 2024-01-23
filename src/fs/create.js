e0361a7d29064e590e0d0c210f26abfa53bcfecd
Asmat1k <dobrovti@mail.ru>
2024-01-23 12:40:26 +0300

    feat: add solution for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const inner = 'I am fresh and young';
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    try  {
+        (await fs).openSync(`${__dirname}/files/fresh.txt`, 'r');
+        console.log('FS operation failed');
+    } catch {
+        try {
+            (await fs).writeFileSync(`src/fs/files/fresh.txt`, inner);
+        } catch (error) {
+            console.log('writeFileSync failed: ', error);
+        }
+    }
 };
 
 await create();
