b1273b58d7f9c40faf1f413656ae78975af21426
Asmat1k <dobrovti@mail.ru>
2024-01-23 16:14:07 +0300

    feat: add delete task solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,7 +3,6 @@ import path from 'path'
 import { fileURLToPath } from 'url';
 
 const copy = async () => {
-    const inner = 'I am fresh and young';
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
 
