5b0154c6d84bb5e78e77c0153b02eaec8bf1988b
Asmat1k <dobrovti@mail.ru>
2024-01-23 16:51:21 +0300

    feat: add args task solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
+import process from 'process'
+
 const parseArgs = () => {
-    // Write your code here 
+    const prArgv = process.argv;
+
+    for(let i = 2; i < prArgv.length - 1; i += 2) {
+        console.log(`${prArgv[i].slice(2)} is ${prArgv[i+1]}`)
+    }
 };
 
 parseArgs();
