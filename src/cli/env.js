555c46c9bd9fa70f76fd3080d12d0bbaa7a1130f
Asmat1k <dobrovti@mail.ru>
2024-01-23 17:01:24 +0300

    feat: add env task solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+import process from 'process'
+
 const parseEnv = () => {
-    // Write your code here 
+    const env = process.env;
+    const entries = Object.entries(env);
+
+    for(const item of entries) {
+        if(item[0].includes('RSS_')) {
+            console.log(`${item[0]}=${item[1]};`);
+        }
+    }
 };
 
 parseEnv();
