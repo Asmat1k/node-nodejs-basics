458d1fd8e4a7b75f94b4f43fd6f6ef85377c53b5
Asmat1k <dobrovti@mail.ru>
2024-01-23 17:50:42 +0300

    feat: add write task solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import process from 'process'
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = `${__dirname}/files/fileToWrite.txt`;
+
+    const writableStream = fs.createWriteStream(filePath, 'utf-8');
+
+    process.stdin.on('data', chunk => {
+        writableStream.write(chunk)
+    });
 };
 
 await write();
