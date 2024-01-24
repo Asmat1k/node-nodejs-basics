457d0e66401ec6d3ae625c3f8b04b38d6b7f87e2
Asmat1k <dobrovti@mail.ru>
2024-01-24 13:28:45 +0300

    feat: add decompress task solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createGzip, createUnzip } from 'node:zlib';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+
 const decompress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const unzip = createUnzip();
+    const read = createReadStream(`${__dirname}/files/archive.gz`);
+    const write = createWriteStream(`${__dirname}/files/fileToCompress.txt`);
+
+    pipeline(read, unzip, write, (error) => {
+        if (error) {
+            console.log('ERROR: ', error);
+        } else {
+            console.log('Archive named "archive.gz" was decompressed into a file!');
+        }
+    })
 };
 
 await decompress();
