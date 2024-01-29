845f9a6d756e357459f918b8a7790d1a9763cb1b
Asmat1k <dobrovti@mail.ru>
2024-01-29 12:14:10 +0300

    fix: fix fs create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,18 +3,20 @@ import path from 'path'
 import { fileURLToPath } from 'url';
 
 const create = async () => {
+    let isError;
     const inner = 'I am fresh and young';
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
 
-    try  {
-        fs.openSync(`${__dirname}/files/fresh.txt`, 'r');
-        throw Error('FS operation failed');
+    try {
+        isError = fs.openSync(`${__dirname}/files/fresh.txt`, 'r');
+        if (isError) throw Error('FS operation failed');
     } catch {
+        if (isError) throw Error('FS operation failed');
         try {
             fs.writeFileSync(`src/fs/files/fresh.txt`, inner);
         } catch (error) {
-             throw Error('writeFileSync failed: ', error);
+            throw Error('FS operation failed with code: ', error);
         }
     }
 };
