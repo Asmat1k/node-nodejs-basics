03f2b845cae794cf0dd29a72cc7d726b77dfe213
Asmat1k <dobrovti@mail.ru>
2024-01-29 12:23:48 +0300

    fix: fix args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,10 +2,13 @@ import process from 'process'
 
 const parseArgs = () => {
     const prArgv = process.argv;
+    let resultStr = '';
 
     for(let i = 2; i < prArgv.length - 1; i += 2) {
-        console.log(`${prArgv[i].slice(2)} is ${prArgv[i+1]}`)
+        resultStr += `${prArgv[i].slice(2)} is ${prArgv[i+1]}, `;
     }
+
+    console.log(resultStr.slice(0, resultStr.length - 2));
 };
 
 parseArgs();
