22e9857f5f39b0688960eb73ba24a0fca974e34b
Asmat1k <dobrovti@mail.ru>
2024-01-29 12:20:53 +0300

    fix: fix cli env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -3,12 +3,15 @@ import process from 'process'
 const parseEnv = () => {
     const env = process.env;
     const entries = Object.entries(env);
+    let resultStr = '';
 
     for(const item of entries) {
         if(item[0].includes('RSS_')) {
-            console.log(`${item[0]}=${item[1]};`);
+            resultStr += `${item[0]}=${item[1]}; `;
         }
     }
+
+    console.log(resultStr);
 };
 
 parseEnv();
